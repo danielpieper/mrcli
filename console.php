0d53c7adecb19ce71168c49e8179835d8c7344fa
Daniel Pieper <daniel@daniel-pieper.com>
2018-12-16 07:23:01 +0100

    Update config, Add services, di
    Configuration using config file was not suitable for a small
    cli app, so this was changed to use environment variables and
    arguments.
    Not sure if dependency injection is really needed here,
    will find out when the slack client is added.

--- console.php
+++ console.php
@@ -4,13 +4,41 @@
 require __DIR__.'/vendor/autoload.php';
 
 use DanielPieper\MergeReminder\Command\ConfigureCommand;
-use DanielPieper\MergeReminder\Command\MergeRequestsCommand;
+use DanielPieper\MergeReminder\Command\DefaultCommand;
 use DanielPieper\MergeReminder\Command\SlackCommand;
+use DanielPieper\MergeReminder\Service\MergeRequestService;
+use DanielPieper\MergeReminder\Service\ProjectService;
 use Symfony\Component\Console\Application;
+
+$dotEnv = new \Dotenv\Dotenv(__DIR__);
+$dotEnv->load();
+$dotEnv->required(['GITLAB_TOKEN'])->notEmpty();
 
 $app = new Application('mrcli', '@package_version@');
 
-$app->add(new ConfigureCommand('configure'));
-$app->add(new SlackCommand('slack'));
-$app->add(new MergeRequestsCommand('merge-requests'));
+$container = new \League\Container\Container();
+$container->add(\Gitlab\Client::class, function () {
+    $gitlabUrl = getenv('GITLAB_URL');
+    if (!$gitlabUrl) {
+        $gitlabUrl = 'https://gitlab.com';
+    }
+    return \Gitlab\Client::create($gitlabUrl)->authenticate(
+        getenv('GITLAB_TOKEN'),
+        \Gitlab\Client::AUTH_URL_TOKEN
+    );
+});
+
+$container->add(MergeRequestService::class)
+    ->addArgument(\Gitlab\Client::class);
+
+$container->add(ProjectService::class)
+    ->addArgument(\Gitlab\Client::class);
+
+$container->add(DefaultCommand::class)
+    ->addArgument(ProjectService::class)
+    ->addArgument(MergeRequestService::class);
+
+
+$app->add($container->get(DefaultCommand::class));
+$app->setDefaultCommand($container->get(DefaultCommand::class)->getName(), true);
 $app->run();
--- src/Command/DefaultCommand.php
+++ src/Command/DefaultCommand.php
@@ -5,17 +5,37 @@ namespace DanielPieper\MergeReminder\Command;
 use DanielPieper\MergeReminder\Service\MergeRequestService;
 use DanielPieper\MergeReminder\Service\ProjectService;
 use DanielPieper\MergeReminder\ValueObject\MergeRequest;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class MergeRequestsCommand extends BaseCommand
+class DefaultCommand extends Command
 {
+    /** @var ProjectService */
+    private $projectService;
+
+    /** @var MergeRequestService */
+    private $mergeRequestService;
+
+    public function __construct(ProjectService $projectService, MergeRequestService $mergeRequestService)
+    {
+        $this->projectService = $projectService;
+        $this->mergeRequestService = $mergeRequestService;
+        parent::__construct();
+    }
+
     protected function configure()
     {
         $this
             ->setName('merge-requests')
-            ->setDescription('List pending merge requests');
+            ->setDescription('List pending merge requests')
+            ->addArgument(
+                'projects',
+                InputArgument::IS_ARRAY | InputArgument::REQUIRED,
+                'Gitlab project id\'s (separate by space)'
+            );
     }
 
     /**
@@ -28,25 +48,15 @@ class MergeRequestsCommand extends BaseCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $config = $this->getConfiguration();
-
-        $client = \Gitlab\Client::create($config['gitlab_url'])->authenticate(
-            $config['gitlab_token'],
-            \Gitlab\Client::AUTH_URL_TOKEN
-        );
-
-        $projectService = new ProjectService($client);
-        $mergeRequestService = new MergeRequestService($client);
-
         $projects = [];
-        foreach ($config['gitlab_projects'] as $gitlabProjectId) {
-            $projects[] = $projectService->get($gitlabProjectId);
+        foreach ($input->getArgument('projects') as $projectId) {
+            $projects[] = $this->projectService->get((int)$projectId);
         }
 
         /** @var MergeRequest[] $mergeRequests */
         $mergeRequests = [];
         foreach ($projects as $project) {
-            $mergeRequests = array_merge($mergeRequests, $mergeRequestService->all($project));
+            $mergeRequests = array_merge($mergeRequests, $this->mergeRequestService->all($project));
         }
 
         $rows = [];
